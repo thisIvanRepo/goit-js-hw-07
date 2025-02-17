70df607bee789c38d2b8e2290ef1e0e3c2970e3d
Ivan_Enko <110199ws@gmail.com>
2025-02-17 11:41:42 +0200

    done according to requirements

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
 const categoryElement = document.querySelector("#categories");
 const itemsCategory = categoryElement.querySelectorAll(".item");
-console.log(`Amount of categories: ${itemsCategory.length}`);
+console.log(`Number of categories: ${itemsCategory.length}`);
 
 itemsCategory.forEach(item => {
   const itemTitle = item.querySelector(".item__title").textContent;
   const countItems = item.querySelectorAll(".subcategory-item").length;
 
-  console.log(`\nCategory ${itemTitle.toUpperCase()} has ${countItems} points`)
+  console.log(`Category: ${itemTitle}`)
+  console.log(`Elements: ${countItems}`)
 })
