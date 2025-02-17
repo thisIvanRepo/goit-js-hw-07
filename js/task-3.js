e882dba0402e16642f62be978c06b2603e748611
Ivan_Enko <110199ws@gmail.com>
2025-02-17 18:18:58 +0200

    task tree done

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const inputValue = document.getElementById("name-input");
+const outputName = document.getElementById("name-output");
+
+inputValue.addEventListener('input', event => {
+  const inputName = capitalizeFirstLetter(event.target.value.trim());
+  outputName.textContent = inputName === "" ? "Anonymous" : inputName;
+})
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
