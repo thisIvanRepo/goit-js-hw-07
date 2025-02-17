c1ca6180016859f42c009da5d900e7315d1b3227
Ivan_Enko <110199ws@gmail.com>
2025-02-18 00:45:01 +0200

    almost completed

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,40 @@
+const containerBoxes = document.querySelector("#boxes");
+const controlPanel = document.querySelector("#controls")
+const imputedElement = document.querySelector("input[type='number']");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", event => {
+  const amount = imputedElement.value;
+  createBoxes(Number(amount));
+  imputedElement.value = "";
+});
+
+destroyButton.addEventListener("click", event => {
+  containerBoxes.innerHTML = "";
+  imputedElement.value = "";
+});
+
+function createBoxes(amount) {
+  if(amount < 0 || amount > 100) {
+    return;
+  } else {
+    let sizeNextBox = 30;
+    let boxes = "";
+
+    for (let i = 1; i <= amount; i += 1) {
+      boxes += `<div
+                  style="background-color: ${getRandomHexColor()};
+                          width: ${sizeNextBox}px;
+                          height: ${sizeNextBox}px;">
+                </div>`
+      sizeNextBox += 10;
+    }
+
+    containerBoxes.insertAdjacentHTML("beforeend", boxes);
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
