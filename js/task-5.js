ea7af391e8ade803022d784713105c2513e89960
Ivan_Enko <110199ws@gmail.com>
2025-02-17 22:56:45 +0200

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const pageBody = document.querySelector('body');
+const changeColorButton = document.querySelector('.change-color');
+const colorCode = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', (event) => {
+  const newHexBackgroundColor = getRandomHexColor();
+  pageBody.style.backgroundColor = newHexBackgroundColor;
+  colorCode.textContent = newHexBackgroundColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
