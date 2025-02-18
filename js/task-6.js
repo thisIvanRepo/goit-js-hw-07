3f44bbcdd15be9c5013c19dce0a10767172c903d
Ivan_Enko <110199ws@gmail.com>
2025-02-18 10:39:57 +0200

    all tasks done

--- js/task-6.js
+++ js/task-6.js
@@ -1,40 +1,40 @@
-const containerBoxes = document.querySelector("#boxes");
-const controlPanel = document.querySelector("#controls")
-const imputedElement = document.querySelector("input[type='number']");
-const createButton = document.querySelector("[data-create]");
-const destroyButton = document.querySelector("[data-destroy]");
+const containerBoxes = document.querySelector('#boxes');
+const imputedElement = document.querySelector('input[type="number"]');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
 
-createButton.addEventListener("click", event => {
+createButton.addEventListener('click', (event) => {
   const amount = imputedElement.value;
-  containerBoxes.style.padding = "32px";
-  createBoxes(Number(amount));
-  imputedElement.value = "";
+
+  if (!amount || amount <= 0 || amount > 100) {
+    return;
+  } else {
+    createBoxes(Number(amount));
+    containerBoxes.style.padding = '32px';
+    imputedElement.value = '';
+  }
 });
 
-destroyButton.addEventListener("click", event => {
-  containerBoxes.innerHTML = "";
-  imputedElement.value = "";
-  containerBoxes.style.padding = "";
+destroyButton.addEventListener('click', (event) => {
+  containerBoxes.innerHTML = '';
+  imputedElement.value = '';
+  containerBoxes.style.padding = '';
 });
 
 function createBoxes(amount) {
-  if(amount < 0 || amount > 100) {
-    return;
-  } else {
-    let sizeNextBox = 30;
-    let boxes = "";
+  let sizeNextBox = 30;
+  let boxes = '';
 
-    for (let i = 1; i <= amount; i += 1) {
-      boxes += `<div
+  for (let i = 1; i <= amount; i += 1) {
+    boxes += `<div
                   style="background-color: ${getRandomHexColor()};
                           width: ${sizeNextBox}px;
                           height: ${sizeNextBox}px;">
-                </div>`
-      sizeNextBox += 10;
-    }
-
-    containerBoxes.insertAdjacentHTML("beforeend", boxes);
+                </div>`;
+    sizeNextBox += 10;
   }
+
+  containerBoxes.insertAdjacentHTML('beforeend', boxes);
 }
 
 function getRandomHexColor() {
